99255a828fdb90ce86c7fe55bc63711015c5c989
Jeanne Lima <jeannelima.profissional@gmail.com>
2023-01-25 06:58:01 -0300

    chore(package.json): configurar scripts de atalho para rodar os testes com cypress

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,7 +1,7 @@
 /// <reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', () => {
-  it('deve exibir no título da aba "Central de Atendimento ao Cliente TAT" quando a página for carregada', function() {
+  it('deve exibir na aba do navegador "Central de Atendimento ao Cliente TAT" como título da aplicação quando a página for carregada', function() {
     cy.visit('./src/index.html')
     cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
   })
