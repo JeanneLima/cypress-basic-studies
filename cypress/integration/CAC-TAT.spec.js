8614fd19cff894f4638ae76d70bb2b0178dee371
Jeanne Lima <23726423+JeanneLima@users.noreply.github.com>
2023-01-29 02:30:52 -0300

    Feature/elements basic events (#3)
    * test(CAC-TAT): add cypress test to form submission flow with mandatory fields filled
    
    * test(CAC-TAT): change long text field filling delay for faster testing
    
    * test(CAC-TAT): add cypress test for form submission flow with invalid email
    
    * test(CAC-TAT): add cypress test to validate rejection of non-numeric value in phone field
    
    * test(CAC-TAT): add cypress test to form submission flow when phone is required and it has not been filled out
    
    * test(CAC-TAT): add cypress test to validate that fields have the typed and cleared value
    
    * test(CAC-TAT): add cypress test to form submission flow when required fields are not filled
    
    * test(CAC-TAT): create custom command with cypress to avoid code repetition
    
    * refactor(CAC-TAT.spec.js): refactor tests to use contains functionality instead of get

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,8 +1,88 @@
 /// <reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', () => {
-  it('deve exibir na aba do navegador "Central de Atendimento ao Cliente TAT" como título da aplicação quando a página for carregada', function() {
+  beforeEach(() => {
     cy.visit('./src/index.html')
+  }); 
+
+  it('deve exibir na aba do navegador "Central de Atendimento ao Cliente TAT" como título da aplicação quando a página for carregada', () => {
     cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+  })
+
+  it('deve exibir mensagem de sucesso quando enviado o formulário com os campos obrigatórios preenchidos', () => {
+    const longText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+
+    cy.get('#firstName').type('Fulano')
+    cy.get('#lastName').type('Silva')
+    cy.get('#email').type('fulano.silva@gmail.com')
+    cy.get('#open-text-area').type(longText, { delay: 0 })
+    cy.contains('button', 'Enviar').click()
+    cy.get('.success').should('be.visible')
+  })
+
+  it('deve exibir mensagem de erro quando submetido o formulário com um email com formatação inválida', () => {
+    cy.get('#firstName').type('Fulano')
+    cy.get('#lastName').type('Silva')
+    cy.get('#email').type('fulanosilva$gmail.com')
+    cy.get('#open-text-area').type('Teste')
+    cy.contains('button', 'Enviar').click()
+    cy.get('.error').should('be.visible')
+  })
+
+  it('deve manter o campo de telefone vazio quando digitado valor não-numérico no mesmo', () => {
+    cy.get('#phone')
+      .type('abc@')
+      .should('have.value', '')
+  })
+
+  it('deve exibir mensagem de erro quando o telefone se torna obrigatório, mas não é preenchido antes do envio do formulário', () => {
+    cy.get('#firstName').type('Fulano')
+    cy.get('#lastName').type('Silva')
+    cy.get('#email').type('fulano.silva@gmail.com')
+    cy.get('#phone-checkbox').click()
+    cy.get('#open-text-area').type('Teste')
+    cy.contains('button', 'Enviar').click()
+    cy.get('.error').should('be.visible')
+  })
+
+  it('deve preencher e limpar os campos "Nome", "Sobrenome", "E-mail" e "Telefone" conforme digitação', () => {
+    const firstName = 'Fulano';
+    const lastName = 'Silva';
+    const email = 'fulano.silva@gmail.com';
+    const phone = '00000000';
+
+    cy.get('#firstName')
+      .type(firstName)
+      .should('have.value', firstName)
+      .clear()
+      .should('have.value', '')
+
+    cy.get('#lastName')
+      .type(lastName)
+      .should('have.value', lastName)
+      .clear()
+      .should('have.value', '')
+
+    cy.get('#email')
+      .type(email)
+      .should('have.value', email)
+      .clear()
+      .should('have.value', '')
+
+    cy.get('#phone')
+      .type(phone)
+      .should('have.value', phone)
+      .clear()
+      .should('have.value', '')
+  })
+
+  it('deve exibir mensagem de erro quando submeter o formulário sem preencher os campos obrigatórios', () => {
+    cy.contains('button', 'Enviar').click()
+    cy.get('.error').should('be.visible')
+  })
+
+  it('deve enviar o formulário com sucesso usando um comando customizado', () => {
+    cy.fillMandatoryFieldsAndSubmit('Ciclano')
+    cy.get('.success').should('be.visible')
   })
 })
