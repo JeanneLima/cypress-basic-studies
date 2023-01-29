5dd7c5d502d0b64fdcadd5c6c20fa54e5a31c1bb
Jeanne Lima <jeannelima.profissional@gmail.com>
2023-01-29 01:10:19 -0300

    test(CAC-TAT): add cypress test to form submission flow with mandatory fields filled

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,8 +1,20 @@
 /// <reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', () => {
-  it('deve exibir na aba do navegador "Central de Atendimento ao Cliente TAT" como título da aplicação quando a página for carregada', function() {
+  beforeEach(() => {
     cy.visit('./src/index.html')
+  }); 
+
+  it('deve exibir na aba do navegador "Central de Atendimento ao Cliente TAT" como título da aplicação quando a página for carregada', () => {
     cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+  })
+
+  it.only('deve preencher os campos obrigatórios e enviar o formulário', () => {
+    cy.get('#firstName').type('Fulano')
+    cy.get('#lastName').type('Silva')
+    cy.get('#email').type('fulano.silva@gmail.com')
+    cy.get('#open-text-area').type('Preciso de X coisa em Y tempo...')
+    cy.get('.button[type="submit"]').click()
+    cy.get('.success').should('be.visible')
   })
 })
